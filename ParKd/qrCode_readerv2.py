c67666303bca4ffe21523b6b1cf009451fae449b
iAM_sAM <58006805+iAM-sAM-R@users.noreply.github.com>
2021-08-22 05:19:44 +1000

    QR_code reader version 2

--- ParKd/qrCode_readerv2.py
+++ ParKd/qrCode_readerv2.py
@@ -1,29 +1,27 @@
 import cv2
 import msvcrt as m
 
-camera = cv2.VideoCapture(0)
+def qrcode_reader():
+    camera = cv2.VideoCapture(0)
+    
+    print("Press enter to scan qr code")
+    m.getch()
+    return_value, image = camera.read()
+    
+    cv2.imwrite('temp.png', image)
+    
+    img = cv2.imread('temp.png')
+    
+    detector = cv2.QRCodeDetector()
+    data, bbox, straight_qrcode = detector.detectAndDecode(img)
+    print("\n\nThe qr code resolved to:\t",data)
+    
+    cv2.imshow("QR_code", img)
+    
+    cv2.waitKey(0) 
+    
+    cv2.destroyAllWindows() 
+    del(camera)
+    
 
-print("Press enter to scan qr code")
-m.getch()
-return_value, image = camera.read()
-
-cv2.imwrite('image.png', image)
-
-img = cv2.imread('image.png')
-
-detector = cv2.QRCodeDetector()
-data, bbox, straight_qrcode = detector.detectAndDecode(img)
-print(data)
-
-cv2.imshow("QR_code", img)
-
-#waits for user to press any key 
-#(this is necessary to avoid Python kernel form crashing)
-cv2.waitKey(0) 
-
-#closing all open windows 
-cv2.destroyAllWindows() 
-
-
-
-del(camera)
+qrcode_reader()
